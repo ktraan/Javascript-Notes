52f6c0efe9d9009c1bfb915fd74a276aa3a0e59b
Kevin Tran <kevintraan@live.com>
2019-09-24 09:49:18 -0600

    Added MemeGenerator Assignment

---
+++ assign-1-starter/js/main.js
@@ -0,0 +1,11 @@
+// Enter your JavaScript for the solution here...
+
+let memeSelect = document.querySelector("select#meme-image");
+let topText = document.querySelector("input#meme-top-text");
+let bottomText = document.querySelector("input#meme-bottom-text");
+let memeImage = document.querySelector("img");
+
+memeSelect.addEventListener("click", (evt) =>
+{
+
+});
