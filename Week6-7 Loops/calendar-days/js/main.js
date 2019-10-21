e4214a94e662059beace912b889dbd01e8c1dd3e
Kevin Tran <kevintraan@live.com>
2019-10-21 10:47:08 -0600

    Added calendar example

---
+++ Week6-7 Loops/calendar-days/js/main.js
@@ -0,0 +1,27 @@
+// array of day names
+let days = [
+    'Sun',
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat'
+];
+
+// variable for the table, which includes a row for day names
+let cal = '<table border="1"><tr>';
+
+for (let idx = 0; idx < days.length; idx += 1) {
+    // add each day name to the row
+    cal += '<th>' + days[idx] + '</th>';
+}
+
+// close the table; ready for display
+cal += '</tr></table>';
+
+document.querySelector('.calendar').innerHTML = cal;
+
+
+let today = new Date();
+console.log("Today is a " + days[today.getDay()]);
