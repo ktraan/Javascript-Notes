df081d84348f67bb249a0da59e8ea1ea0534a5e0
Kevin Tran <kevintraan@live.com>
2019-10-22 09:43:45 -0600

    .

--- Assign-2/js/main.js
+++ Assign-2/js/main.js
@@ -4,27 +4,16 @@ let thumbnailTextArray = Array.from(thumbnailText);
 let imageList = document.querySelectorAll("img");
 let imageArray = Array.from(imageList);
 
-	
-
-
 
 input.addEventListener("input", event =>
 {
 
-	if (input.value.length > 0)
+	for (let count; count < thumbnailTextArray.length; count++)
 	{
-		for (let count; count < thumbnailTextArray.length; count++)
-		{
-			thumbnailTextSearch = thumbnailTextArray[count].innerHTML;
-			
-			if (thumbnailTextArray[count].innerHTML.includes(input.value))
-			{
-				
-			}
-
-		}
-
+		
 	}
 
 
+
 });
+
