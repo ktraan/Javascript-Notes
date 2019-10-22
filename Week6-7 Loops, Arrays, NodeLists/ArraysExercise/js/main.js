916853d5f063fd7d53ad7229e09c86aaaf24e323
Kevin Tran <kevintraan@live.com>
2019-10-22 08:51:49 -0600

    Added exception in loops exercise

--- Week6-7 Loops, Arrays, NodeLists/ArraysExercise/js/main.js
+++ Week6-7 Loops, Arrays, NodeLists/ArraysExercise/js/main.js
@@ -8,18 +8,26 @@ let button = document.querySelector('.carousel');
 button.addEventListener('click', (evt) =>
 {
 	let target = evt.target;
+
 	if (target.classList.contains('control'))
 	{
 		if (target.classList.contains('next'))
 		{
-			// move to the next index of the array
 			currentImg += 1;
 		}
 		else if (target.classList.contains('prev'))
 		{
-			// move to previous index of array
 			currentImg -= 1;
 		}
+		if (currentImg > 2)
+		{
+			currentImg = 0;
+		}
+		else if (currentImg < 0)
+		{
+			currentImg = 2;
+		}
+
 		let newImage = document.querySelector('.carousel>img').src = 'images/' + images[currentImg]
 	}
 })
