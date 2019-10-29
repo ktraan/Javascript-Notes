df1ef07fd09258fa5593226bf3ac43dfd88670e0
Kevin Tran <kevintraan@live.com>
2019-10-29 17:25:08 -0600

    Finished assignment 2

--- Assign-2/js/main.js
+++ Assign-2/js/main.js
@@ -1,20 +1,43 @@
 let input = document.querySelector("input");
+let divArray = document.querySelectorAll(".thumb-display");
+let imageArray = Array.from(divArray);
 
-let div = document.querySelectorAll(".thumb-display");
-let divArray = Array.from(div);
-let hiddenDiv = ".thumb-display.hidden"
-
-input.addEventListener("input", event =>
+input.addEventListener("input", (evt) =>
 {
-	for (let count = 0; count < divArray.length; count++)
+	for (let count = 0; count < imageArray.length; count++)
 	{
-		if (!divArray[count].innerText.includes(input.value))
+		let imageText = imageArray[count].innerText;
+		if (imageText.indexOf(input.value) < 0)
 		{
-			divArray[count].className = hiddenDiv;
+			imageArray[count].className = "thumb-display hidden";
+		}
+		else
+		{
+			imageArray[count].className = "thumb-display";
 		}
 
+		if (input.value == "")
+		{
+			reset.className = "reset hidden";
+		}
+		else
+		{
+			reset.className = "reset";
+		}
+	}
+});
+
+
+let reset = document.querySelector(".reset");
+
+reset.addEventListener("click", (evt) =>
+{
+	evt.preventDefault();
+	input.value = "";
+	reset.className = "reset hidden";
+	for (let count = 0; count < imageArray.length; count++)
+	{
+		imageArray[count].className = "thumb-display";
 	}
 
-	
-};
-
+});
